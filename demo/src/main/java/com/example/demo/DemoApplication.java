fea4968847f8cf14eddb74545d442dffe4de3761
Nattasak Chonmanat <nattasak.cho@dome.tu.ac.th>
2025-05-11 18:16:35 +0700

    Adasdas

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,9 @@ package com.example.demo;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
@@ -15,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 				description = "API FOR CS251 PROJECT"
 		)
 )
+@EnableAutoConfiguration
 public class DemoApplication {
 
 	public static void main(String[] args) {
